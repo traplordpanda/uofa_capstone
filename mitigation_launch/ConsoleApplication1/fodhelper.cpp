15c6ca2e0cb8efb416d8b26b08124d4dbf69942c
kyle aubuchon <kdaub.tf2@gmail.com>
2023-04-13 20:41:34 -0400

    Changed the game

--- mitigation_launch/ConsoleApplication1/fodhelper.cpp
+++ mitigation_launch/ConsoleApplication1/fodhelper.cpp
@@ -1,15 +1,24 @@
 import launch_utils; 
+import mitigations;
 
 #include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <string_view>
 
-int main()
+int main(int argc, char* argv[])
 {
-    DWORD64 mitigation_flags = 
-        PROCESS_CREATION_MITIGATION_POLICY_FONT_DISABLE_MASK | 
-        PROCESS_CREATION_MITIGATION_POLICY_HIGH_ENTROPY_ASLR_ALWAYS_ON | 
-        PROCESS_CREATION_MITIGATION_POLICY2_MODULE_TAMPERING_PROTECTION_ALWAYS_ON | 
-        PROCESS_CREATION_MITIGATION_POLICY2_MODULE_TAMPERING_PROTECTION_DEFER;
-
+    DWORD64 mitigation_flags{ 0 };
+    std::vector<std::string_view> arguments(argv+1, argv + argc);
+    for ( const auto& arg: arguments ){
+        if (auto mitigation = mitigation_map.find(arg);  mitigation == mitigation_map.end())
+        {
+            std::cout << "Mitigation " << arg << " not found\n";
+            return 1;
+        }
+        else {
+            mitigation_flags = mitigation->second | mitigation_flags;
+        }
+    }
     auto pl = launch_notepad(mitigation_flags);
-
 }
